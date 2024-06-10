991b8628c90187bf81f8f44f13ccf7e8a438c8de
Dirk Uys <108363293+JackSpanner@users.noreply.github.com>
2024-06-10 20:42:31 +0200

    Update config.lua (#2)
    Added additional configs

--- config.lua
+++ config.lua
@@ -1,6 +1,7 @@
 Config = {}
-
+Config.RollingBurnout = true ---Allows linelock to end for a rolling burnout when close to tire temps
+Config.Limiter = true --- allows vehicle to hit engine limiter on rolling burnout
 Config.Cars = {
     --[GetHashKey("vehicle spawn name")] = {launchrpm = rpm the vehicle launches at, tractionControlValue = traction loss number(higher the number, higher the traction loss), lowSpeedTractionControlValue = low speed traction loss value(higher the number, higher the traction loss)
-    [GetHashKey("adder")] = {launchrpm = 8000, tractionControlValue = 3, lowSpeedTractionControlValue = 1.5},
+    [GetHashKey("comet7")] = {launchrpm = 5000, tractionControlValue = 3, lowSpeedTractionControlValue = 1.5},
 }
